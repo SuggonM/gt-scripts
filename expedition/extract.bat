1da399a5d979f92843c859fbeb955c945104463d
Suggon <maharjansagun99@gmail.com>
2024-02-29 19:04:05 +0545

    also send error response status

--- expedition/extract.bat
+++ expedition/extract.bat
@@ -10,7 +10,7 @@ set _UNPACKER_PATH="C:\UNPACKER_PATH\texture-unpacker"
 
 
 pushd %_UNPACKER_PATH%
-if errorlevel 1 start notepad %0 & pause & exit /b
+if errorlevel 1 start notepad %0 & pause & exit /b 1
 call npm run unpack "%~dp0%Note" json
 popd
 pause
