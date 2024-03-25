70d9e5062ec0f4f767a37c08ca9b8881acaa1857
Suggon <maharjansagun99@gmail.com>
2024-03-25 08:31:50 +0545

    minimize repetition

--- X-FILES/img-download.mjs
+++ X-FILES/img-download.mjs
@@ -1,19 +1,28 @@
 import fs from 'fs';
 import { request } from 'https';
 
-const inFile = './X-FILES_links.json';
-const outFile = './X-FILES_names.txt';
+const title = 'X-FILES';
+
+const inFile = title + '_links.json';
+const logFile = title + '_names.txt';
+const outFolder = title + '/';
 
 const urlFile = fs.readFileSync(inFile);
 const urls = JSON.parse(urlFile);
 
-const logFile = fs.createWriteStream(outFile);
+const logFileStream = fs.createWriteStream(logFile);
 
-urls.forEach((url) => downloadFile(url));
+try {
+    urls.forEach((url) => downloadFile(url));
+} catch(e) {
+    console.error(e);
+} finally {
+    console.log(`LOG: ${urls.length} images downloaded inside folder ./${outFolder}`);
+}
 
 function logImgName(url, name) {
     const lineData = `${url} : ${name}\n`;
-    logFile.write(lineData);
+    logFileStream.write(lineData);
 }
 
 function downloadFile(url) {
@@ -32,7 +41,7 @@ function downloadFile(url) {
         const CD = headers[CDindex];
         const rawname = decodeURI(CD);
         const name = rawname.replace(/.*UTF-8''/, '');
-        const dlpath = fs.createWriteStream('./X-FILES/' + name);
+        const dlpath = fs.createWriteStream(outFolder + name);
         logImgName(url, name);
         response.pipe(dlpath);
     });
