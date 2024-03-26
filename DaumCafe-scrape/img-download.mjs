4d2bb08bbff0b1b0030b3d60bc9a7d446a7c5d3c
Suggon <maharjansagun99@gmail.com>
2024-03-26 15:11:31 +0545

    img-download: for security, added validateHost and uniqueName

--- DaumCafe-scrape/img-download.mjs
+++ DaumCafe-scrape/img-download.mjs
@@ -16,6 +16,11 @@ const urls = JSON.parse(urlFile);
 
 const logFileStream = fs.createWriteStream(logFile);
 
+// some images have duplicate naming and silently corrupt the downloading image
+// list names and check uniqueness as solution
+const imgList = [];
+let dupeCount = 0;
+
 try {
     urls.forEach((url) => downloadFile(url));
     console.log(`LOG: Downloading ${urls.length} images into folder "./${outFolder}" ...`);
@@ -23,12 +28,8 @@ try {
     console.error(e);
 }
 
-function logImgName(url, name) {
-    const lineData = `${url} : ${name}\n`;
-    logFileStream.write(lineData);
-}
-
 function downloadFile(url) {
+    url = validateHost(url);
     const urlpath = url.split('/');
     const hostname = urlpath[2];
     const path = '/' + urlpath.slice(3).join('/');
@@ -39,15 +40,54 @@ function downloadFile(url) {
     }
 
     const httpsreq = request(options, (response) => {
-        const headers = response.rawHeaders;
-        const CDindex = headers.indexOf('Content-Disposition') + 1;
-        const CD = headers[CDindex];
-        const rawname = decodeURI(CD);
-        const name = rawname.replace(/.*UTF-8''/, '');
-        const dlpath = fs.createWriteStream(outFolder + name);
+        const headers = response.headers;
+
+        const CD = headers["content-disposition"];
+        const nameFragment = CD.replace(/.*UTF-8''/, '');
+        const name = decodeURI(nameFragment);
+
+        const CType = headers["content-type"];
+
+        const uniqueName = getUniqueName(name, CType);
+        const dlpath = fs.createWriteStream(outFolder + uniqueName);
         logImgName(url, name);
         response.pipe(dlpath);
     });
 
     httpsreq.end();
+}
+
+function validateHost(url) {
+    const secureHost = 't1.daumcdn.net/cfile/cafe/';
+    const insecureHost = /cfile\d+\.uf\.daum\.net\/image\//;
+    // "cfile[0-9]{3}.uf.daum.net/image/"
+
+    let secureUrl = url;
+    if (url.match(insecureHost)) {
+        const last4 = /.{4}$/;
+        secureUrl = url.replace(insecureHost, secureHost).replace(last4, '');
+        // "https://cfile293.uf.daum.net/image/99B5034C5F0287A63F30B6"
+        //  "https://t1.daumcdn.net/cfile/cafe/99B5034C5F0287A63F"
+    }
+
+    return secureUrl;
+}
+
+function getUniqueName(name, CType) {
+    const noExtName = name.substring(0, name.lastIndexOf('.')) || name;
+    const ext = CType.replace('image/', '');
+
+    if (imgList.includes(noExtName)) {
+        const dupeName = `${noExtName} (${++dupeCount}).${ext}`;
+        return dupeName;
+    }
+
+    imgList.push(noExtName);
+    const uniqueName = `${noExtName}.${ext}`;
+    return uniqueName;
+}
+
+function logImgName(url, name) {
+    const lineData = `${url} : ${name}\n`;
+    logFileStream.write(lineData);
 }
